7f133f277889e575df10b24e8380cd35301165fd
Ernst <ernst.oldenhof@gmail.com>
2018-03-12 21:05:52 +0100

    IMplemented Azure SQL call for premium request

--- models/premium.py
+++ models/premium.py
@@ -33,35 +33,115 @@ from project_config.config import (sql_parameter_tablename_dict,
                                   AZURE_DATABASE,
                                   AZURE_PORT)
 
-
+valid_pricing_parameter_type_names = ['country', 'airport', 'container type']
 # NB: base_rate should go into the db
 
-def testquery_azure(premium_request):
-    """
-    Test function for Azure SQL
 
+driver= '{ODBC Driver 13 for SQL Server}'
+
+def calculate_premium(premium_request):
     """
-    driver= '{ODBC Driver 13 for SQL Server}'
-    #cnxn = pyodbc.connect('DRIVER='+driver+';PORT='+port+'1433;SERVER='+server+';PORT=1443;DATABASE='+database+';UID='+username+';PWD='+ password)
-    cnxn = pyodbc.connect((r"DRIVER={driver};PORT={port};SERVER={server};"
+    :premium_request : a dictionary
+
+    Looks into the database to search for the parameters, and appends the
+    results to a DataFrame.
+    Returns the quote amount, and a list of dictionaries with detailed info
+    """
+    ## Assert that all required keys to calculate a premium are provided
+    for key in required_keys:
+        assert key in premium_request.keys(), ('Aborting: key "{}" missing in '
+                                            'request'.format(key))
+
+    ## Get values from request: timestamp, amount insured, product_id
+    # timestamp
+    try:
+        # The following works when a list is given of [Y, M, D]
+        timestamp = datetime(*premium_request['timestamp'])
+    except:
+        # When input is a string: 'yyyy-mm-dd'
+        timestamp = datetime.strptime(premium_request['timestamp'], '%Y-%m-%d')
+
+    # To filter valid_from/to (not implemented yet)
+    timestamp_str = datetime.strftime(timestamp, '%Y-%m-%d')
+
+    # product_id and amount
+    product_id = premium_request['product_id']
+    sum_insured = premium_request['sum_insured']
+
+    # init the list that contains the elements that determine the premium
+    quote_reply = [{'sum_insured': sum_insured,
+                   'product_id': product_id,
+                   'timestamp': timestamp_str}]
+
+    # Loop through all items, to search for the "parameters"/premium multipliers
+    for key, parameter_ids in premium_request.items():
+
+        # Test if this is a key that requires a parameter lookup
+        # if not, skip it
+        if key in valid_pricing_parameter_type_names:
+            sql = """ SELECT * FROM [dbo].[Pricing_Param_Ref_Data] WHERE
+                    Reference_Data_ID = ? and Product_ID = ?
+                  """
+
+            # Make database connection
+            db_connection = pyodbc.connect((r"DRIVER={driver};PORT={port};SERVER={server};"
                            r"DATABASE={database};UID={username};PWD={password}").format(driver=driver,
                                                     port=AZURE_PORT,
                                                     server=AZURE_SERVER,
                                                     database=AZURE_DATABASE,
                                                     username=AZURE_USERNAME,
                                                     password=AZURE_PASSWORD))
-    cursor = cnxn.cursor()
-    # cursor.execute("SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE='BASE TABLE'")
-    cursor.execute('SELECT * FROM Reference_Data_Value WHERE Reference_Data_ID=575')
-    row = cursor.fetchone()
-    cnxn.close()
-    quote_reply = [{'date':'01-01-0000',
-                    'amount':row[9],
-                    'text': row[6],
-                    'type':'TEST_RESULT',
-                    'parameter_id': 575,
-                    'factor': '1.23456' }]
-    return (1234, quote_reply)
+            # Create database cursor
+            cursor = db_connection.cursor()
+            try:
+                for parameter_id in parameter_ids:
+                    vals = (parameter_id, product_id) #Timestamp not used yet!
+                    cursor.execute(sql, vals)
+
+                    # fetch the result
+                    columns = [column[0] for column in cursor.description]
+
+                    # find the index of the row where the Pricing Parameter Value is located
+                    # NB: those strings should probably be in config
+                    pricing_param_value_index = [i for i, col_name in enumerate(columns)
+                                       if col_name == 'Pricing_Parameter_Value'][0]
+                    ref_data_name_index = [i for i, col_name in enumerate(columns)
+                                       if col_name == 'Reference_Data_Name'][0]
+                    pricing_param_type_name_index = [i for i, col_name in enumerate(columns)
+                                       if col_name == 'Pricing_Parameter_Type_Name'][0]
+
+                    # Get the values
+                    row = cursor.fetchone()
+                    pricing_param_value = row[pricing_param_value_index]
+                    ref_data_name = row[ref_data_name_index]
+                    pricing_param_type_name = row[pricing_param_type_name_index]
+
+                    # Append dictionaries to the list
+
+
+                    quote_reply.append({'type': key,
+                                        'pricing_param_type_name': pricing_param_type_name,
+                                        'pricing_param_value': pricing_param_value,
+                                        'ref_data_name': ref_data_name})
+            except Exception as e:
+                print(e)
+                return {'message': ('parameter_id {} for key {} for date'
+                                '{} not found').format(
+                                    parameter_id, key, timestamp_str)}
+            finally:
+                db_connection.close()
+        else:
+            continue # for clarity
+
+    # Process the obtained data to calculate the premium
+    parameters = [item['pricing_param_value'] for item in quote_reply if 'pricing_param_value' in item.keys() ]
+    # Multiply all parameters
+    parameters_product = 1
+    for par in parameters:
+        parameters_product *= par
+    premium = (premium_request['sum_insured'] * base_rate * parameters_product)
+    return (premium, quote_reply)
+
 
 # def calculate_premium(premium_request):
 #     """
@@ -143,9 +223,34 @@ def testquery_azure(premium_request):
 #     parameters_product = npproduct(parameters)
 #     premium = (premium_request['amount'] * base_rate * parameters_product)
 #     return (premium, quote_reply)
-
-
-
+#
+#
+#
+# def testquery_azure(premium_request):
+#     """
+#     Test function for Azure SQL
+#
+#     """
+#     driver= '{ODBC Driver 13 for SQL Server}'
+#     cnxn = pyodbc.connect((r"DRIVER={driver};PORT={port};SERVER={server};"
+#                            r"DATABASE={database};UID={username};PWD={password}").format(driver=driver,
+#                                                     port=AZURE_PORT,
+#                                                     server=AZURE_SERVER,
+#                                                     database=AZURE_DATABASE,
+#                                                     username=AZURE_USERNAME,
+#                                                     password=AZURE_PASSWORD))
+#     cursor = cnxn.cursor()
+#     # cursor.execute("SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE='BASE TABLE'")
+#     cursor.execute('SELECT * FROM Reference_Data_Value WHERE Reference_Data_ID=575')
+#     row = cursor.fetchone()
+#     cnxn.close()
+#     quote_reply = [{'date':'01-01-0000',
+#                     'amount':row[9],
+#                     'text': row[6],
+#                     'type':'TEST_RESULT',
+#                     'parameter_id': 575,
+#                     'factor': '1.23456' }]
+#     return (1234, quote_reply)
 
 
 ######################################################
@@ -157,11 +262,12 @@ def do_tests():
 
 
 if __name__ == '__main__':
-    premium_request = {
-                    'amount' : 360E3,
-                    'ground_handlers' : [1, 5, 10],
-                    'airports' : [650, 652, 890],
-                    'timestamp' : (2018, 1, 7)} #Y, M, D
+    premium_request = { 'product_id': 1,
+                        'sum_insured' : 360E3,
+                        'container type' : [1147],
+                        'airport' : [651, 652, 890],
+                        'timestamp' : (2018, 1, 7),
+                          'country': [1217]} #Y, M, D
     premium, quote_reply = calculate_premium(premium_request)
     print(premium)
     print(quote_reply)
--- project_config/config.py
+++ project_config/config.py
@@ -18,8 +18,7 @@ AZURE_DATABASE = os.environ['AZURE_DATABASE']
 table_name_list = ['MAPPING_PAR_REF_AIRPORT', 'MAPPING_PAR_REF_GROUND_HANDLER',
                    'PAR_AIRPORT', 'PAR_GROUND_HANDLER']
 
-required_keys = ['amount', 'ground_handlers',
-                 'airports', 'timestamp']
+required_keys = ['sum_insured', 'timestamp', 'product_id']# Removed airports, ground_handlers
 
 # Define namedtuple that links the table that contains the ID's and
 # the parameter (multiplier) ID's with that that contains the parameters
@@ -29,6 +28,9 @@ TableNames = namedtuple('TableNames', 'table_a table_b column_name')
 # Define the dict with the namedtuple TableNames as value, keys are those that
 # should be present in the request
 sql_parameter_tablename_dict = {}
+
+# The following definitions are a duplicate from SQL.
+# Leave only the keys, get rest from
 sql_parameter_tablename_dict['airports'] = TableNames('MAPPING_PAR_REF_AIRPORT',
                             'PAR_AIRPORT',
                             'AIRPORT_ID')
