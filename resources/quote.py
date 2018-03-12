4f830e251d3e0ef3406b6178daf89d41f2b4b963
Ernst <ernst.oldenhof@gmail.com>
2018-03-12 23:04:05 +0100

    Some clean-up in resources/quote and models/premium, additions to .gitignore

--- resources/quote.py
+++ resources/quote.py
@@ -43,13 +43,11 @@ class Quote(Resource):
 
         premium_request = cls.parser.parse_args() #premium_request is a dict
         quote_reply = None
-
-
         reply = calculate_premium(premium_request)
         try:
             premium, quote_reply = reply
         except TypeError as e:
-            # If there is only a single return item, some error is returned
+            # If there is only a single return item, an error has occurred
             # This is communicated by a message
             message = calculate_premium(premium_request)
 
@@ -58,49 +56,6 @@ class Quote(Resource):
                     "details": quote_reply}, 200
         else:
             return message, 400
-
-# class Quote_test(Resource):
-#     """
-#     Test resource that gets some arbitrary data from Azure to verify that the database
-#     is accessible
-#     """
-#     # Define class attributes
-#     # parser for the JSON, to ensure the form of the JSON payload
-#     parser = reqparse.RequestParser()
-#     parser.add_argument('amount', type=float, required=True,
-#                         help="'Amount' cannot be left blank")
-#     parser.add_argument('ground_handlers', type=int, action='append',
-#                         required=True,
-#                         help="'Ground handlers' cannot be left blank")
-#     parser.add_argument('airports', type=int, action='append', required=True,
-#                         help="'Airports' cannot be left blank")
-#     parser.add_argument('timestamp', type=str, required=True,
-#                         help="'Timestamp' cannot be left blank")
-#
-#     @classmethod
-#     # @jwt_required() # Authentication required
-#     def post(cls):
-#         # First check if this is a valid post ("Error-first approach")
-#         # NB: next takes the first element from the generator
-#
-#         premium_request = cls.parser.parse_args() #premium_request is a dict
-#         quote_reply = None
-#         try:
-#             # premium, quote_reply = calculate_premium(premium_request)
-#             premium, quote_reply = testquery_azure(premium_request)
-#         except Exception as e:
-#             print(e)
-#             pass
-#             # If quote_reply is missing, this means some error occurred
-#             # This is communicated by a message
-#             # NB: UNCOMMENT next line after test!
-#             # message = calculate_premium(premium_request)
-#
-#         if not quote_reply is None:
-#             return {"premium": premium,
-#                     "details": quote_reply}, 200
-#         else:
-#             return message, 400
 
 if __name__ == '__main__':
     pass
